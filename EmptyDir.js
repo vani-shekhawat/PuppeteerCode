5275cb698d9fe3db3fba4e56040c4a225c7b4e8c
Vani shekhawat <singhshekhawat160@gmail.com>
2020-02-01 02:57:11 -0800

    second puppeteer code commit

---
+++ EmptyDir.js
@@ -0,0 +1,16 @@
+module.exports = async function(path)
+{
+    var fs = require("fs");
+	fs.readdir(path,(err,files)=>
+	{
+		if(err) throw err;
+		for(var file of files)
+		{
+			fs.unlink(path +'\\'+ file, err=>
+		  	{
+		  		if(err) throw err;
+		  	});
+		}
+	})
+
+}
