70c989e784f10d05c5ccd33424aa6da5838b2293
vani shekhawat <singhshekhawat160@gmail.com>
2020-02-07 17:26:54 +0530

    data format threw call another api

--- SendCustomerDetail1.js
+++ SendCustomerDetail1.js
@@ -4,7 +4,6 @@ var fs = require('fs');
 var jwt= require('jsonwebtoken');
 var adobereseller = require('./adobereseller');
 var bodyParser = require('body-parser');
-var call = require('./CallApi')
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 app.use(function(req,res, next){
 	res.header("Access-Control-Allow-Origin", "*");
@@ -15,13 +14,13 @@ app.use(function(req,res, next){
 app.get('/adobe', isAuthorized,(req,res)=>
 {
 	console.log(req.query.vip);
-	adobereseller(req.query.vip).then(async function(vip_data)
+	adobereseller(req.query.vip).then(function(vip_data)
 	{
-		var ret_data = await dataFormat(vip_data);
-		res.send(ret_data);
+		res.send(vip_data);
 	});
 
 	
+
 })
 
 app.get('/jwt',(req,res)=>
@@ -57,42 +56,3 @@ var server = app.listen(8081, function () {
 	var port = server.address().port
 	console.log("Example app listening at http://%s:%s", host, port)
 })
-
-async function dataFormat(data)
-{
-	var host = 'https://abhay.aisplestore.com/api/v1';
-	var token= await authentication(host);
-	var ret_data= await getData(host,token, data);
-	return ret_data;
-}
-
-async function authentication(host)
-{
-	endpoint = '/authenticate';
-	formData= {email:'akhil@aisplglobal.com',password:'akhil@987'};
-  	identifier_key = "token";
-	var data=await call(url=host+endpoint,formData, identifier_key);
-	if(data.hasOwnProperty(identifier_key))
-	{
-		return data.token;
-	}
-	else
-	{
-		throw data.error;
-	}
-}
-async function getData(host,token, data)
-{
-	var endpoint=`/getformatcsvdatatojson?token=${token}`;
-	formData= {data:data};
-	identifier_key = "personal_detail";
-	var data_res=await call(url=host+endpoint,formData,identifier_key);
-	if(data_res.hasOwnProperty(identifier_key))
-	{
-		return data_res;
-	}
-	else
-	{
-		throw data_res.error;
-	}
-}
